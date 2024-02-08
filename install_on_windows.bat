10793327e984cbee70842e99b5c114e4b9320da7
Seagullie <59409233+Seagullie@users.noreply.github.com>
2024-02-08 18:04:38 +0200

    Add install script

---
+++ install_on_windows.bat
@@ -0,0 +1,7 @@
+rem create a virtual environment
+python -m venv ../ParseSchedules
+rem activate the virtual environment
+call ./Scripts/activate
+rem install the required packages
+pip install -r requirements.txt
+
