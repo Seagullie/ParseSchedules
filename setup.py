d1a226b5b97b8a3dc7e6aca1c4d728ccf778436a
Seagullie <59409233+Seagullie@users.noreply.github.com>
2024-02-09 15:30:48 +0200

    Add ParseSchedules package to setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+packages = find_packages()
+
+print(packages)
+
 setup(
     name='ParseSchedules',
     version='1.0.0',
     author='Seagullie@GitHub',
     description='Extracts .json representations of schedules from .docx files',
     
-    packages=find_packages(),
+    packages= packages + ["ParseSchedules"],
     install_requires=requirements,
 )
