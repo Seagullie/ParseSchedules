a60f0006feab0c9a9efcfda91b70cc00ab21f315
Seagullie <59409233+Seagullie@users.noreply.github.com>
2025-02-24 19:28:22 +0200

    Change encoding of requirements.txt to UTF-8

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+with open("./requirements.txt", encoding="utf-8") as f:
+    file_contents = f.read()
+    requirements = file_contents.splitlines()
 
 packages = find_packages()
 
 print(packages)
 
 setup(
-    name='ParseSchedules',
-    version='1.0.0',
-    author='Seagullie@GitHub',
-    description='Extracts .json representations of schedules from .docx files',
-    
-    packages= packages,
+    name="ParseSchedules",
+    version="1.0.0",
+    author="Seagullie@GitHub",
+    description="Extracts .json representations of schedules from .docx files",
+    packages=packages,
     install_requires=requirements,
 )
